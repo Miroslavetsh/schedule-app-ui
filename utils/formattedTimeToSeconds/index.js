006bc1fa7e11d305bad44d611c39da80f971f8f8
Miroslavetsh <dfgshte@gmail.com>
2022-04-29 16:31:14 +0300

    Remove utils

--- utils/formattedTimeToSeconds/index.js
+++
@@ -1,5 +0,0 @@
-export default (time) => {
-  const [hrs, mns] = time.split(':')
-
-  return +hrs * 3600 + +mns * 60
-}
--- utils/padTimeWithZeros/index.js
+++
@@ -1,8 +0,0 @@
-const pad = (amount) => (amount < 10 ? '0' + amount : amount)
-
-export default (time) => {
-  return time
-    .split(':')
-    .map((tt) => pad(tt))
-    .join(':')
-}
