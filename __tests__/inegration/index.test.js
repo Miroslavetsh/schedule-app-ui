da7d28891eb9779f825c07331d801d8f18d7ddb0
Miroslavetsh <dfgshte@gmail.com>
2022-05-03 18:14:11 +0300

    Fix type of authorization day value:)

--- __tests__/inegration/index.test.js
+++ __tests__/inegration/index.test.js
@@ -2,10 +2,10 @@ import main from '../../main.js'
 
 describe('App works correctly', () => {
   it('View shows us current day', () => {
-    expect(main('djsaFnGDLk727uiogu3i710gDDA9GhdLIk').currentDay).toBe
+    expect(main('djsaFnGDLk727uiogu3i710gDDA9GhdLIk', 0, '09:00').currentDay).toBe
   })
 
   it('View shows us current time', () => {
-    expect(main('djsaFnGDLk727uiogu3i710gDDA9GhdLIk').currentTime).toBe
+    expect(main('djsaFnGDLk727uiogu3i710gDDA9GhdLIk', 0, '09:00').currentTime).toBe
   })
 })
